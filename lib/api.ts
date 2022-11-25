af611338349ddc9492f0d728c91cefae4b278098
Nicolas Guarini <nikiguarincia@gmail.com>
2022-11-25 20:30:20 +0100

    [Fix] Remove console logging

--- lib/api.ts
+++ lib/api.ts
@@ -44,6 +44,5 @@ export const getAllProjects = (fields: string[] = []) => {
         .map((slug) => getProjectBySlug(slug, fields))
         // sort projects by date in descending ordeer
         .sort((project1, project2) => (Date.parse(project1.started) > Date.parse(project2.started) ? -1 : 1))
-    console.log(projects)
     return projects
 }
