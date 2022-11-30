baad03e871d2f46a89a4a0029447b293fb2ac2fe
Nicolas Guarini <nikiguarincia@gmail.com>
2022-11-30 15:05:58 +0100

    Install and setup wow.js for animating UI components

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,12 +1,19 @@
 import Footer from "./Footer"
 import Meta from "./Meta"
 import Navbar from "./Navbar"
+import { useEffect } from "react"
 
 type Props = {
     children: React.ReactNode
 }
 
 const Layout = ({children}: Props) => {
+    useEffect(() => {
+        const isServer: boolean = typeof window === 'undefined'
+        const WOW = !isServer ? require('wow.js') : null  
+        new WOW().init()
+    }, [])
+
     return (
         <>
             <Meta />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="stylesheet" href="/css/animate.css"/>
+      </Head>
       <body className='bg-black text-white font-robotomono'>
         <Main />
         <NextScript />
