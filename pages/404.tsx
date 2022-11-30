83fb15cad34e256f17bb984ade2483d39c0aa18e
Nicolas Guarini <nikiguarincia@gmail.com>
2022-11-30 15:54:38 +0100

    Add meta tags to 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,13 @@
+import Head from 'next/head'
 import React from 'react'
 
 const FourOhFour = () => {
   return (
     <div className='h-screen w-screen flex justify-center items-center text-md md:text-lg'>
+        <Head>
+          <title>404 - this page could not be found</title>
+          <meta name="robots" content="noindex, nofollow" />
+        </Head>
         <span className='pr-5 mr-5 border-r'>404</span>this page could not be found
     </div>
   )
