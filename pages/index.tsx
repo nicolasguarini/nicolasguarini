c1526c4698c3c2f21f4766d01eff006b63b9642f
Nicolas Guarini <nikiguarincia@gmail.com>
2023-01-31 14:25:18 +0100

    Decrease delay in typing animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,12 @@ export default function Home({ allProjects }: Props) {
         <div className="text-center text-3xl font-thin text-gray absolute top-[40%] p-6 md:px-40 lg:px-80">
           <TypeWriter 
             onInit={(typewriter: TypewriterClass) => {
-              typewriter.typeString("I make webapps, mobile apps, UI/UX design, data analysis and more;").start()
+              typewriter.typeString("I make webapps, mobile apps, UI/UX design, data analysis and more;")
+                .start()
+            }}
+            
+            options={{
+              delay: 75
             }}
           />
         </div>
