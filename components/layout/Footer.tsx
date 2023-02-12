516da9978baff47e3b503d3a918e97561b7fbbb5
Nicolas Guarini <nikiguarincia@gmail.com>
2023-02-12 11:32:05 +0100

    Update footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -11,23 +11,24 @@ const Footer = () => {
       <div className="p-12 pb-12 md:pb-3 px-0 grid gird-cols-1 lg:grid-cols-5">
         <div className="pb-10 text-center md:text-left lg:col-span-2 wow fadeInUp" data-wow-delay='0.08s'>
           <div className="text-lightgray mb-1">connect with me</div>
-          <div>in @nicolasguarini</div>
+          <div><a href="https://www.linkedin.com/in/nicolasguarini/" target={"_blank"}>in @nicolasguarini</a></div>
         </div>
 
         <div className="pb-12 text-center md:text-right wow fadeInUp" data-wow-delay='0.16s'>
-          <div className="text-lightgray mb-1">follow me</div>
-          <div>
+          <div className="text-lightgray mb-1">links</div>
+          <div><a href="/privacy-policy" target={"_blank"}>privacy policy</a></div>
+          {/* <div>
             <ul>
               <li className="inline"><span className="text-[#0A66C2]">•</span> in</li>
               <li className="inline pl-5"><span className="text-[#EB0091]">•</span> ig</li>
               <li className="inline pl-5"><span className="text-[#1D9BF0]">•</span> tw</li>
             </ul>
-          </div>
+          </div> */}
         </div>
 
         <div className="pb-0 text-center md:text-right lg:col-span-2 wow fadeInUp" data-wow-delay='0.24s'>
           <div className="text-lightgray mb-1">say hello</div>
-          <div>nicolasguarini.py@gmail.com</div>
+          <div><a href="mailto:nicolasguarini.py@gmail.com" target={"_blank"}>nicolasguarini.py@gmail.com</a></div>
         </div>
       </div>
 
