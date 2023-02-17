384e1d606054f50db444b949bb2fe862b3d15ecd
Nicolas Guarini <nikiguarincia@gmail.com>
2023-02-17 21:39:15 +0100

    [Fix] @next/next/no-html-link-for-pages, react/jsx-no-tar

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 
 const Footer = () => {
   return (
@@ -11,12 +12,12 @@ const Footer = () => {
       <div className="p-12 pb-12 md:pb-3 px-0 grid gird-cols-1 lg:grid-cols-5">
         <div className="pb-10 text-center md:text-left lg:col-span-2 wow fadeInUp" data-wow-delay='0.08s'>
           <div className="text-lightgray mb-1">connect with me</div>
-          <div><a href="https://www.linkedin.com/in/nicolasguarini/" target={"_blank"}>in @nicolasguarini</a></div>
+          <div><Link href="https://www.linkedin.com/in/nicolasguarini/" target={"_blank"} rel="noreferrer">in @nicolasguarini</Link></div>
         </div>
 
         <div className="pb-12 text-center md:text-right wow fadeInUp" data-wow-delay='0.16s'>
           <div className="text-lightgray mb-1">links</div>
-          <div><a href="/privacy-policy" target={"_blank"}>privacy policy</a></div>
+          <div><Link href="/privacy-policy">privacy policy</Link></div>
           {/* <div>
             <ul>
               <li className="inline"><span className="text-[#0A66C2]">•</span> in</li>
@@ -28,7 +29,7 @@ const Footer = () => {
 
         <div className="pb-0 text-center md:text-right lg:col-span-2 wow fadeInUp" data-wow-delay='0.24s'>
           <div className="text-lightgray mb-1">say hello</div>
-          <div><a href="mailto:nicolasguarini.py@gmail.com" target={"_blank"}>nicolasguarini.py@gmail.com</a></div>
+          <div><Link href="mailto:nicolasguarini.py@gmail.com" target={"_blank"} rel="noreferrer">nicolasguarini.py@gmail.com</Link></div>
         </div>
       </div>
 
