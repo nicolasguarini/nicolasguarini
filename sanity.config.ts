943e892af0ab821d334332dc28352ce39c324f2a
Nicolas Guarini <nikiguarincia@gmail.com>
2024-08-03 15:59:22 +0200

    Use env variables instead of hardcoded project id and dataset

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,14 +2,15 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './sanity/schemas'
+import { dataset, projectId } from './sanity/env'
 
 export default defineConfig({
   basePath: '/studio',
   name: 'default',
   title: 'nicolasguarini.it',
 
-  projectId: "qtdomht6",
-  dataset: "production",
+  projectId: projectId,
+  dataset: dataset,
 
   plugins: [structureTool(), visionTool()],
 
