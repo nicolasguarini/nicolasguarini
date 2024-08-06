170968275341c6198cdb538309bc01e8c03cbbf2
Nicolas Guarini <nikiguarincia@gmail.com>
2024-08-06 21:50:47 +0200

    Update navbar animations

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import ExternalLink from "../icons/externalLink";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,9 +10,19 @@ const Navbar = () => {
   const pathName = usePathname();
 
   useEffect(() => {
+    console.log(pathName); // '/', '/projects/client', '/projects/personal', '/blog', '/about', '/contact', ...
     setIsProjectsOpen(false);
     setIsOpen(false);
   }, [pathName]);
+
+  const linkClasses = (path: string) =>
+    pathName === (path) ? "text-white" : "text-[#A1A1A1]";
+
+  const linkClassesContains = (path: string) =>
+    pathName.includes(path) ? "text-white" : "text-[#A1A1A1]";
+
+  const linkClassesOr = (path1: string, path2: string) =>
+    pathName === (path1) || pathName.includes(path2) ? "text-white" : "text-[#A1A1A1]";
 
   return (
     <nav className="bg-black text-white">
@@ -23,16 +34,16 @@ const Navbar = () => {
             </Link>
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-5">
-                <Link href="/" className="text-[#A1A1A1]">
+                <Link href="/" className={linkClasses("/")}>
                   Home
                 </Link>
-                <Link href="/about" className="text-[#A1A1A1]">
+                <Link href="/about" className={linkClasses("/about")}>
                   About
                 </Link>
                 <div className="relative">
                 <button
                   onClick={() => setIsProjectsOpen(!isProjectsOpen)}
-                  className="text-[#A1A1A1] focus:outline-none flex items-center"
+                  className={linkClassesContains("/projects") + " focus:outline-none flex items-center"}
                 >
                   Projects
                   <svg className="ml-1 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
@@ -40,12 +51,16 @@ const Navbar = () => {
                   </svg>
                 </button>
                   {isProjectsOpen && (
-                    <div className="absolute mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                    <div className="absolute mt-2 w-48 rounded-md shadow-lg bg-gray-900 ring-1 ring-black ring-opacity-5" style={{
+                      backgroundColor: "rgb(3, 5, 15, 0.65)",
+                      backdropFilter: "blur(8px)",
+                      WebkitBackdropFilter: "blur(8px)",
+                    }}>
                       <div className="py-1">
-                        <Link href="/projects/client" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+                        <Link href="/projects/client" className={"block px-4 py-2 " + linkClasses("/projects/client")}>
                           Client Projects
                         </Link>
-                        <Link href="/projects/personal" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
+                        <Link href="/projects/personal" className={"block px-4 py-2 " + linkClasses("/projects/personal")}>
                           Personal Projects
                         </Link>
                       </div>
@@ -53,20 +68,18 @@ const Navbar = () => {
                   )}
                 </div>
 
-                <Link href="/blog" className="text-[#A1A1A1]">
+                <Link href="/blog" className={linkClassesContains("/blog")}>
                   Blog
                 </Link>
-                <Link href="/notes" className="text-[#A1A1A1]">
-                  Notes
-                </Link>
-                <Link href="https://github.com/nicolasguarini" className="text-[#A1A1A1]">
+                <Link href="https://github.com/nicolasguarini" target="_blank" className="text-[#A1A1A1]">
                   GitHub
+                  <ExternalLink />
                 </Link>
               </div>
             </div>
           </div>
           <div className="hidden md:block">
-            <Link href="/contact" className="px-5 py-3 border border-[#333232] rounded-lg">
+            <Link href="/contact" className="px-5 py-3 border border-[#333232] hover:border-[#A1A1A1] rounded-lg transition-all">
               Contact
             </Link>
           </div>
@@ -102,30 +115,33 @@ const Navbar = () => {
             WebkitBackdropFilter: "blur(8px)",
           }}
         >
-          <Link href="/" className="block px-3 py-2">
+          <Link href="/" className={linkClasses("/") + " block px-3 py-2"}>
             Home
           </Link>
 
-          <Link href="/about" className="block px-3 py-2">
+          <Link href="/about" className={linkClasses("/") + " block px-3 py-2"}>
             About
           </Link>
-          <Link href="/projects/clients" className="block px-3 py-2">
+          <Link href="/projects/clients" className={linkClasses("/") + " block px-3 py-2"}>
             Client Projects
           </Link>
-          <Link href="/projects/personal" className="block px-3 py-2">
+          <Link href="/projects/personal" className={linkClasses("/") + " block px-3 py-2"}>
             Personal Projects
           </Link>
-          <Link href="/blog" className="block px-3 py-2">
+          <Link href="/blog" className={linkClasses("/") + " block px-3 py-2"}>
             Blog
           </Link>
-          <Link href="/resume" className="block px-3 py-2">
-            Resume
-          </Link>
-          <Link href="/notes" className="block px-3 py-2">
-            Notes
-          </Link>
-          <Link href="/contact" className="block px-3 py-2">
+          
+          <Link href="/contact" className={linkClasses("/") + " block px-3 py-2"}>
             Contact
+          </Link>
+          <Link href="https://nicolasguarini.notion.site/Appunti-Universit-e5cefc29fff04a3283799023b7558337?pvs=74" target="_blank" className="block px-3 py-2">
+            Notes
+            <ExternalLink />
+          </Link>
+          <Link href="https://github.com/nicolasguarini" target="_blank" className="block px-3 py-2">
+            GitHub
+            <ExternalLink />
           </Link>
         </div>
       </div>
