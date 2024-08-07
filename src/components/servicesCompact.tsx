b30036f281b89b28e5bea0fb8715b96f4e69f4a9
Nicolas Guarini <nikiguarincia@gmail.com>
2024-08-08 00:13:11 +0200

    Update icons

--- src/components/servicesCompact.tsx
+++ src/components/servicesCompact.tsx
@@ -1,6 +1,10 @@
 import Button from "./button";
+import AppIcon from "./icons/appIcon";
 import BagIcon from "./icons/bagIcon";
+import CartIcon from "./icons/cartIcon";
+import DesignIcon from "./icons/designIcon";
 import EducationIcon from "./icons/educationIcon";
+import WorldIcon from "./icons/worldIcon";
 
 export default function ServicesCompact() {
     return (
@@ -21,24 +25,24 @@ export default function ServicesCompact() {
 
             <div className="flex flex-wrap gap-10">
                 <div className="flex flex-col gap-3 sm:basis-[46%] xl:basis-[22%]">
-                    <BagIcon />
+                    <AppIcon />
                     <h3 className="font-bold text-xl">Web and Mobile Apps</h3>
                     <p>
                         Development of progressive web apps and native / cross-platform mobile applications. I also take care of the infrastructure and database design, and backend development.
                     </p>
                 </div>
                 <div className="flex flex-col gap-3 sm:basis-[46%] xl:basis-[22%]">
-                    <EducationIcon />
+                    <WorldIcon />
                     <h3 className="font-bold text-xl">Websites</h3>
                     <p>Websites and landing pages, custom-developed and tailored to the specific needs of the businesses I work with. Each site includes a content management panel, basic SEO optimization, and analytics services.</p>
                 </div>
                 <div className="flex flex-col gap-3 sm:basis-[46%] xl:basis-[22%]">
-                    <BagIcon />
+                    <CartIcon />
                     <h3 className="font-bold text-xl">E-Commerce</h3>
                     <p>E-commerce sites developed with specific technologies based on the volume of products and sales to be managed, either from scratch or by integrating the functionalities into an existing site.</p>
                 </div>
                 <div className="flex flex-col gap-3 sm:basis-[46%] xl:basis-[22%]">
-                    <EducationIcon />
+                    <DesignIcon />
                     <h3 className="font-bold text-xl">UI/UX Design</h3>
                     <p>Often included in other services, I take care of the visual design and experience of the products I build. My style is modern, elegant and minimal (as I imagine you can see from this site).</p>
                 </div>
