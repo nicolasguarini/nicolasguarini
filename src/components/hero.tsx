3526afc4abe32c72de344c97fa003a7bdef5acce
Nicolas Guarini <nikiguarincia@gmail.com>
2024-08-07 16:03:01 +0200

    Update site identity

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,8 +5,7 @@ export default function Hero() {
         <div className="flex flex-col items-center justify-center py-28 lg:py-40 gap-8 bg-hero">
             <h1 className="text-4xl font-bold text-center max-w-4xl">I craft Websites, eCommerce, and Apps that deliver results.</h1>
             <p className="max-w-2xl text-center">
-                Experienced freelance developer with a Computer Science degree.
-                Delivering top-notch digital solutions for small businesses for over five years.
+            I design, plan and develop exceptional digital products, supporting my clients in the entire innovation process that includes the development of custom software.
             </p>
             <div className="flex flex-row items-center justify-center gap-6">
                 <Button href="/portfolio" variant="primary">Portfolio</Button>
