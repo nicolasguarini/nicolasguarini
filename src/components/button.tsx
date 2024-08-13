5793cb1b1565c79198b8a48c81411e9ea1c567ea
Nicolas Guarini <nikiguarincia@gmail.com>
2024-08-13 14:35:55 +0200

    Add simple animations to buttons and images

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,8 +2,18 @@ import Link from "next/link";
 
 export default function Button({ href, variant, children }: { href: string, variant: string, children: React.ReactNode }) {
     return (
-        <Link href={href} className={`block py-2 px-6 rounded-lg border transition-all ${variant !== "outline" ? 'bg-[#EDEDED] text-black ' : 'bg-transparent border-[#333232] hover:border-white'}`}>
-            {children}
+        <Link href={href} className={`relative block py-2 px-6 rounded-lg border transition-all overflow-hidden 
+            ${variant !== "outline" ? 
+                'bg-[#EDEDED] text-black' : 
+                'bg-transparent border-[#333232] hover:border-white'
+            } 
+            before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 
+            ${variant !== "outline" ? 
+                'before:bg-black' : 
+                'before:bg-[#333232]'
+            } 
+            before:transition-all before:duration-500 hover:text-white hover:before:w-full`}>
+            <span className="relative z-10">{children}</span>
         </Link>
     )
 }
--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -2,12 +2,18 @@ import { urlFor } from "@/src/sanity/lib/utils";
 import CategoryTag from "./categoryTag";
 import Link from "next/link";
 
-export default function ProjectCard({ project }: { project: any}) {
+export default function ProjectCard({ project }: { project: any }) {
     return (
         <Link href={`/projects/${project.type}/${project.slug}`}>
-            <div className="flex flex-col gap-3 max-w-sm xl:max-w-md"> 
+            <div className="flex flex-col gap-3 max-w-sm xl:max-w-md">
                 {project.featuredImage && (
-                    <img className="h-full w-full rounded-xl object-cover" src={urlFor(project.featuredImage).url()} alt="" />
+                    <div className="overflow-hidden rounded-xl">
+                        <img 
+                            className="h-full w-full object-cover transform transition-transform duration-500 ease-in-out hover:scale-110" 
+                            src={urlFor(project.featuredImage).url()} 
+                            alt="" 
+                        />
+                    </div>
                 )}
                 <h3 className="font-bold text-xl">{project.name}</h3>
                 <p>{project.excerpt}</p>
@@ -21,6 +27,5 @@ export default function ProjectCard({ project }: { project: any}) {
                 </p>
             </div>
         </Link>
-       
     )
 }
